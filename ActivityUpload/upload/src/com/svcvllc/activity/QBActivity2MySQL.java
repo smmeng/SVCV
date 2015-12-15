da93f786c9aa942c9a35bb9abf34fdeebc372570
smmeng <smmeng@gmail.com>
2015-12-14 22:56:17 -0800

    -Update QBActivity2MySQL.java to share same method for fetching investors -Replace system.out.println with logger. -Include logging jar.

--- ActivityUpload/upload/src/com/svcvllc/activity/QBActivity2MySQL.java
+++ ActivityUpload/upload/src/com/svcvllc/activity/QBActivity2MySQL.java
@@ -19,9 +19,12 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 
 public class QBActivity2MySQL {
-    //   private final static ADFLogger logger = ADFLogger.createADFLogger(QBActivity2MySQL.class);
+    private static final Log logger=LogFactory.getLog(QBActivity2MySQL.class);
     final static String QBFile = "C:\\Users\\smeng1\\Downloads\\qb.csv";
     final static String QBInterestFile = "C:\\Users\\smeng1\\Downloads\\qb-int.csv";
     final static String MySQLString = "jdbc:mysql://www.svcvllc.com:3306/security", MySQLUser = "svcvllc", MySQLPassword ="intelinside";
@@ -33,13 +36,16 @@ public class QBActivity2MySQL {
         super();
     }
     
+    /************************************************************************************************************************************
+     * Upload investment principal activities from QB.csv
+     ************************************************************************************************************************************/
     static public void uploadPrincipalActivity() {
         java.util.Date today = new java.util.Date();
         java.sql.Date timestamp = new java.sql.Date(today.getTime());
         
         FileInputStream fstream = null;
         BufferedReader br = null;
-        HashMap svcvUserMap = new HashMap();
+        HashMap svcvUserMap ;
 
         String strLine;
 
@@ -48,43 +54,22 @@ public class QBActivity2MySQL {
             br = new BufferedReader(new InputStreamReader(fstream));
             //Read File Line By Line
         } catch (Exception e) {
-            System.err.println(e.getMessage()); // handle exception
+            logger.error(e.getMessage()); // handle exception
         }
-        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+        logger.info("-------- MySQL JDBC Connection Demo ------------");
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found !!");
+            logger.info("MySQL JDBC Driver not found !!");
             return;
         }
-        System.out.println("MySQL JDBC Driver Registered!");
+        logger.info("MySQL JDBC Driver Registered!");
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(MySQLString, MySQLUser, MySQLPassword);
-            System.out.println("SQL Connection to database established!");
-
-            // Get users first from a readonly query
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT  username, id,email FROM auth_user");
-            while (rs.next()) {
-
-                ArrayList user = new ArrayList();
-                user.add(rs.getObject(2));
-                user.add(rs.getObject(3));
-                svcvUserMap.put(rs.getObject(1).toString().toUpperCase(), user);
-        //                for (int i = 1; i <= numColumns; i++) {
-                    // Column numbers start at 1.
-                    // Also there are many methods on the result set to return
-                    //  the column as a particular type. Refer to the Sun documentation
-                    //  for the list of valid conversions.
-                    System.out.print("id=["+rs.getObject(2) +"]\tuserName-[" +  rs.getObject(1)+ "]\t\temail=["+rs.getObject(3));
-        //                }
-                System.out.println("");
-            }
-            rs.close();
-            statement.close();
-
+            logger.info("SQL Connection to database established!");
+            svcvUserMap = findAllUser(connection);
 
             // Insert user transactions into the activity table
             PreparedStatement pstmt = null;
@@ -94,7 +79,7 @@ public class QBActivity2MySQL {
             int lineCounter = 0;
             while ((strLine = br.readLine()) != null) {
                 // Print the content on the console
-                System.out.println(strLine);
+                logger.info(strLine);
 
                 if (null == strLine || strLine.isEmpty())
                     continue;
@@ -138,16 +123,16 @@ public class QBActivity2MySQL {
 
             //connection.commit();  // auto-commit is on
         } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
+            logger.info("Connection Failed! Check output console");
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("Read Failed!");
+            logger.info("Read Failed!");
             e.printStackTrace();
         } finally {
             try {
                 if (connection != null)
                     connection.close();
-                System.out.println("Connection closed !!");
+                logger.info("Connection closed !!");
 
                 if (br != null) {
                     try {
@@ -164,6 +149,9 @@ public class QBActivity2MySQL {
         
     }
 
+    /************************************************************************************************************************************
+     * Upload interest activities from QB-int.csv
+     ************************************************************************************************************************************/
     static public void uploadInterestActivity() {
         
         java.util.Date today = new java.util.Date();
@@ -171,7 +159,7 @@ public class QBActivity2MySQL {
         
         FileInputStream fstream = null;
         BufferedReader br = null;
-        HashMap svcvUserMap = new HashMap();
+        HashMap svcvUserMap;
 
         String strLine;
 
@@ -180,22 +168,24 @@ public class QBActivity2MySQL {
             br = new BufferedReader(new InputStreamReader(fstream));
             //Read File Line By Line
         } catch (Exception e) {
-            System.err.println(e.getMessage()); // handle exception
+            logger.error(e.getMessage()); // handle exception
         }
-        System.out.println("-------- MySQL JDBC Connection for Interest ------------");
+        logger.info("-------- MySQL JDBC Connection for Interest ------------");
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found !!");
+            logger.info("MySQL JDBC Driver not found !!");
             return;
         }
-        System.out.println("MySQL JDBC Driver Registered!");
+        logger.info("MySQL JDBC Driver Registered!");
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(MySQLString, MySQLUser, MySQLPassword);
-            System.out.println("SQL Connection to database established!");
+            logger.info("SQL Connection to database established!");
 
+            svcvUserMap = findAllUser(connection);
+/**
             // Get users first from a readonly query
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("SELECT  username, id,email FROM auth_user");
@@ -210,13 +200,13 @@ public class QBActivity2MySQL {
                     // Also there are many methods on the result set to return
                     //  the column as a particular type. Refer to the Sun documentation
                     //  for the list of valid conversions.
-                    System.out.print("id=["+rs.getObject(2) +"]\tuserName-[" +  rs.getObject(1)+ "]\t\temail=["+rs.getObject(3));
+                    logger.info("id=["+rs.getObject(2) +"]\tuserName-[" +  rs.getObject(1)+ "]\t\temail=["+rs.getObject(3));
         //                }
-                System.out.println("");
+                logger.info("");
             }
             rs.close();
             statement.close();
-
+**/
 
             // Insert user transactions into the activity table
             PreparedStatement pstmt = null;
@@ -226,11 +216,11 @@ public class QBActivity2MySQL {
             int lineCounter = 0;
             while ((strLine = br.readLine()) != null) {
                 // Print the content on the console
-                System.out.println(strLine);
+                logger.info(strLine);
 
                 if (null == strLine || strLine.isEmpty())
                     continue;
-                String[] fields = strLine.split(",");// not ," as separator
+                String[] fields = strLine.split(",\"");// not ," as separator
 
                 if (lineCounter == 0 || fields.length < 3 || null == fields[2] || fields[2].isEmpty() || strLine.toUpperCase().indexOf("CANCEL") > 0) {
                     lineCounter++;
@@ -240,23 +230,23 @@ public class QBActivity2MySQL {
                 lineCounter++;
 
                 String[] activityDate = fields[2].split("/");
-                String projectId = fields[5];
+                String projectId = fields[4];
                 projectId = projectId.substring(projectId.indexOf(ProjectStr), projectId.length());
                 projectId = projectId.substring(ProjectStr.length(), projectId.indexOf("-"));
 
-                String userName = fields[4].substring(fields[4].indexOf("(") + 1, fields[4].indexOf(")"));
+                String userName = fields[3].substring(fields[3].indexOf("(") + 1, fields[3].indexOf(")"));
                 ArrayList user = (ArrayList)svcvUserMap.get(userName.toUpperCase());
                 Integer userId = (Integer)user.get(0);
                 
-                String memo = fields[5].replaceAll("\"", ""); 
+                String memo = fields[4].replaceAll("\"", ""); 
                 memo = memo.replaceFirst(ProjectStr + projectId+"-", "");
 
-                String[] amount = fields[7].split(",");
+                String[] amount = fields[5].split(",");
                 pstmt.setInt(1, 0);
                 pstmt.setString(2, "Interest");//fields[1].replaceAll("\"", ""));
                 pstmt.setString(3, activityDate[2] + "-" + activityDate[0] + "-" + activityDate[1]); //
                 pstmt.setString(4, memo); //
-                pstmt.setFloat(5, Float.parseFloat(amount[0])); //
+                pstmt.setFloat(5, Float.parseFloat(amount[1])); //
                 pstmt.setInt(6, Integer.parseInt(projectId)); //
                 pstmt.setInt(7, userId.intValue()); //
                 pstmt.setDate(8, timestamp); //
@@ -270,16 +260,16 @@ public class QBActivity2MySQL {
 
             //connection.commit();  // auto-commit is on
         } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
+            logger.info("Connection Failed! Check output console");
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("Read Failed!");
+            logger.info("Read Failed!");
             e.printStackTrace();
         } finally {
             try {
                 if (connection != null)
                     connection.close();
-                System.out.println("Connection closed !!");
+                logger.info("Connection closed !!");
 
                 if (br != null) {
                     try {
@@ -294,6 +284,58 @@ public class QBActivity2MySQL {
             }
         }
         
+    }
+    
+    /************************************************************************************************************************************
+     * prepare a list of investors from MySQL before uploading any activites
+     ************************************************************************************************************************************/
+    static private HashMap findAllUser(Connection connection) {
+        HashMap svcvUserMap = null;
+        Statement statement = null;
+        ResultSet rs = null;
+
+        try
+        {
+            // Get users first from a readonly query
+            statement = connection.createStatement();
+            rs = statement.executeQuery("SELECT  username, id,email FROM auth_user");
+            svcvUserMap = new HashMap();
+            while (rs.next()) {
+    
+                ArrayList user = new ArrayList();
+                user.add(rs.getObject(2));
+                user.add(rs.getObject(3));
+                svcvUserMap.put(rs.getObject(1).toString().toUpperCase(), user);
+            //                for (int i = 1; i <= numColumns; i++) {
+                    // Column numbers start at 1.
+                    // Also there are many methods on the result set to return
+                    //  the column as a particular type. Refer to the Sun documentation
+                    //  for the list of valid conversions.
+                    logger.info("id=["+rs.getObject(2) +"]\tuserName-[" +  rs.getObject(1)+ "]\t\temail=["+rs.getObject(3));
+            //                }
+                logger.info("");
+            }
+            rs.close();
+            statement.close();
+        
+        } catch (SQLException e) {
+            logger.info("Connection Failed! Check output console");
+            e.printStackTrace();
+        }
+        finally {
+            try
+            {
+                if (rs != null)
+                    rs.close();
+                
+                if (statement != null)
+                    statement.close();
+            
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return svcvUserMap;
     }
 
     public static void main(String[] args) {
