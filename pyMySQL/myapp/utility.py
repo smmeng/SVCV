15799981cf6752e125893febea95043a4ea5a473
smmeng <smmeng@gmail.com>
2015-12-20 23:06:30 -0800

    Bug fix for regular user

--- pyMySQL/myapp/utility.py
+++ pyMySQL/myapp/utility.py
@@ -23,7 +23,7 @@ class Utility:
             user = request.user
             myId =  request.user.id
             activeUsers = InvestmentActivity.objects.values('UserId').distinct()
-            #print 'viewable_user_list() activeUsers:  ', activeUsers
+            print 'viewable_user_list() activeUsers:  ', activeUsers
             
             if user.is_superuser: #for superusers. They can't view super users' records but staffs' records 
                 allUser_list = User.objects.values('id', 'username', 'first_name', 'last_name', 'is_active').order_by('first_name').filter(id__in=activeUsers)
@@ -32,15 +32,19 @@ class Utility:
                 #activeUsers.append({'UserId':myId})
                 allUser_list = User.objects.values('id', 'username','first_name', 'last_name', 'is_active').order_by('first_name').filter(Q(id__in=activeUsers, is_superuser=False, is_staff=False)|Q(id=myId))
                 viewableUserId_list = User.objects.values('id').filter(Q(id__in=activeUsers, is_superuser=False, is_staff=False)|Q(id=myId))
+            else: # The scope is only himself/herself
+                print "regular user [", user
+                allUser_list = User.objects.values('id', 'username','first_name', 'last_name', 'is_active').filter(id=myId)
+                viewableUserId_list = User.objects.values('id').filter(id=myId)
 
             for user in allUser_list:
                 uid = user['id']
                 uname = user['username']
                 allUserId_dict[uid] = user
                 allUserName_dict[uname]=user
-                
-            #print 'viewable_user_list() allUser_list:', allUser_list, ' \n allUserId_list=', viewableUserId_list, ' \n allUserId_dict=', allUserId_dict
-            #print 'viewable_user_list() allUserId_dict=', allUserId_dict
+                    
+            print 'viewable_user_list() allUser_list:', allUser_list, ' \n allUserId_list=', viewableUserId_list, ' \n allUserId_dict=', allUserId_dict
+            print 'viewable_user_list() allUserId_dict=', allUserId_dict
         except InvestmentActivity.DoesNotExist:
         # We get here if we didn't find the specified project.
         # Don't do anything - the template displays the "no project" message for us.
