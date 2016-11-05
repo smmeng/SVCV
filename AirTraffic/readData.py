ab755f89b35923251cdce38c33bbf2ab3b8575f5
smmeng <smmeng@gmail.com>
2016-11-04 23:35:20 -0700

    Finalize both readData py

--- AirTraffic/readData.py
+++ AirTraffic/readData.py
@@ -1,9 +1,21 @@
+#!/usr/bin/python
 import sys
 import telnetlib
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("lab2.svcvllc.com","datafeed","Sunnyva1e","AirTraffic" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
 
 tn_ip = "localhost"
 tn_port = "30003"
+minimumHeight=8000
+interval=5
+noGPSInterval= interval *3
 
+flightMap = {}
 #Access the local port`
 def telnet():
     try:
@@ -16,11 +28,47 @@ def telnet():
 		line = tn.read_until("\r\n")
 		#print "=>", line
 		tokens = line.split(',')
+		#skip various conditions
 		if (len(tokens)) < 17:
 			continue
-		if (len(tokens[14]) == 0 or len(tokens[15]) == 0 ):
+		if (len(tokens[11]) > 0 and int(tokens[11])> minimumHeight):   #skip the high fliers
 			continue
-		print  "=>", tokens[0], tokens[1], tokens[4], tokens[6], tokens[7], tokens[8],tokens[9], tokens[11], tokens[14], tokens[15]
+		#if (len(tokens[14]) == 0 or len(tokens[15]) == 0 or len(tokens[11]) == 0 ): #skip one has no 3D 
+		#	continue
+		#print  "=>", tokens[0], tokens[1], tokens[4], tokens[6], tokens[7], tokens[8],tokens[9], tokens[10], tokens[11], tokens[14], tokens[15]	
+		icao = tokens[4]
+		#if (flightMap[icao] is None):
+			#flightMap[icao] = tokens[7]
+		time = tokens[6] + " " + tokens[7]
+		sql=""
+		try:
+			# Execute the SQL command
+			if (int(tokens[1]) ==3):
+				alt="0"
+				if (len(tokens[11]) > 0):
+					alt = tokens[11]
 
+				lat="0.00"
+				if (len(tokens[14]) > 0):
+					lat = tokens[14]
+
+				long="0.00"
+				if (len(tokens[15]) > 0):
+					long = tokens[15]
+					
+				sql = "INSERT INTO positionHistory VALUES(null,'" + tokens[4] +"','" + time + "', " 
+				#print "SQL=", sql
+				sql = sql +alt + ", " + lat + ", " + long +  "," + tokens[1] + ")"
+				#print "SQ2L=", sql
+				cursor.execute(sql)
+			# Commit your changes in the database
+			db.commit()
+		except:
+			# Rollback in case there is any error
+			print "MySQL Insert errort time=[", time, "], SQL=", sql
+			db.rollback()
 
 telnet()
+
+db.close()
+
--- AirTraffic/readJSONData.py
+++ AirTraffic/readJSONData.py
@@ -2,8 +2,22 @@
 import urllib2 
 import json 
 import time
+from datetime import datetime
+import MySQLdb
 
-url = 'http://savemysky.svcvllc.com:8000/data/aircraft.json'
+# Open database connection
+db = MySQLdb.connect("lab2.svcvllc.com","datafeed","Sunnyva1e","AirTraffic" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+#url = 'http://savemysky.svcvllc.com:8000/data/aircraft.json'
+url = 'http://localhost:8080/data/aircraft.json'
+minimumHeight=8000
+interval=10
+noGPSInterval= interval *3
+noGPSAltDiff = 200
+
 while True:
 	req = urllib2.Request(url) 
 	opener = urllib2.build_opener() 
@@ -12,7 +26,8 @@ while True:
 	#print json 
 	#print json['aircraft']
 	aircraftList = jsonData['aircraft']
-
+	now = str(datetime.now())
+	
 	for flit in aircraftList:
 		hex = flit.get("hex", "")
 		speed = flit.get("speed", "0")
@@ -31,7 +46,23 @@ while True:
 		rssi = flit.get("rssi", "")
 		#mlat = flit.get("mlat", "")
 		#tisb = flit.get("tisb", "")
-
-		if (float(lat) > 0 and float(lon)!=0):
+		sql = ""
+		if (float(lat) > 0 and float(lon)!=0 and int(altitude) < minimumHeight):
 			print hex, flight, altitude, lat, lon, speed, squawk, seen_pos, vert_rate, track
+			sql += "INSERT INTO postionJSONHistory VALUES(null,'" + hex +"','" +flight + "', '"
+			sql += now[0:19] + "', " + str(altitude) + ", " + str(lat) + ", "+ str(lon) + ", '" + squawk + "', " + str(nucp) + ", "
+			sql += str(seen_pos) + ", " + str(vert_rate) + ", "+ str(track) + ", " + str(speed)+ ", '" + category + "', " 
+			sql += str(messages) + ", " + str(seen) + ", " + str(rssi) + ")"
+			#
+			#print "SQL=", sql
+			try:
+				cursor.execute(sql)
+				# Commit your changes in the database
+				db.commit()
+			except:
+				# Rollback in case there is any error
+				print "MySQL Insert errort time=[", time, "], SQL=", sql
+				db.rollback()
+
 	time.sleep(10)
+
