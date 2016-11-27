f42eca26a4fd2babecc9bb9a6a7ca5d31d02eb93
smmeng <smmeng@gmail.com>
2016-11-27 00:08:39 -0800

    Output badplane json to html

--- AirTraffic/AirtrafficUI/static/js/app.js
+++ AirTraffic/AirtrafficUI/static/js/app.js
@@ -176,19 +176,16 @@ app.controller('appGMapsCtrl', function($scope, $filter, $http) {
 });
 
 var app = angular.module('myFixedHeadApp', []);
-
 app.controller('fixedHeadCtrl', function($scope, $filter, $http) {
 	var jsonURL = "/showBadPlaneData/";
-	var homeLatlng = new google.maps.LatLng(37.3522374, -122.052706);
-	$scope.mapProp = {
-			    center:homeLatlng,
-			    zoom:11,
-			    mapTypeId:google.maps.MapTypeId.ROADMAP
-			   };
-
-	$scope.map=new google.maps.Map(document.getElementById("map_canvas"),$scope.mapProp);
 	
     $http.get(jsonURL).success(function (data) {
+    	$scope.flights = data;
+    	for(var i = 0; i < $scope.flights.length; i++)
+    	{
+    		row = $scope.flights[i];
+    		console.log("row[" + i  + "]="+ row.Flight);
+    	};
     })
 });
     
